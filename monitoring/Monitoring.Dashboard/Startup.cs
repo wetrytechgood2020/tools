578ae5b5aaf08a1c3ecb6270fc83aba9b648dcc1
PGrasseels <grasseelsp@hotmail.com>
2020-06-05 19:10:11 +0200

    Update Riofile & Monitoring

---
+++ monitoring/Monitoring.Dashboard/Startup.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Monitoring.Dashboard
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+              .AddHealthChecksUI();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting()
+                .UseEndpoints(config =>
+                {
+                    config.MapHealthChecksUI(ui =>
+                    {
+                        ui.UIPath = "/";
+                    });
+                });
+        }
+    }
+}
